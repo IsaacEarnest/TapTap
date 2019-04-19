d10a590d6693fb769d3910b44422cf2a386b3cf5
RaderF <36457068+RaderF@users.noreply.github.com>
2019-04-18 20:43:36 -0500

    Added play_hitclap
    new method inside SoundTestActivity

--- app/src/main/java/com/example/osumania/SoundTestActivity.java
+++ app/src/main/java/com/example/osumania/SoundTestActivity.java
@@ -2,10 +2,10 @@ package com.example.osumania;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
 
 
 public class SoundTestActivity extends Activity {
@@ -16,6 +16,17 @@ public class SoundTestActivity extends Activity {
             setContentView(R.layout.test_sound_activity);
 
             Button one = (Button)this.findViewById(R.id.button1);
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.normalhitclap);
+            one.setOnClickListener(new OnClickListener(){
+
+                public void onClick(View v) {
+                    mp.start();
+                }
+            });
+        }
+
+        // Rename to play_given_sound when sounds are passable
+        public void play_hitclap(Button one){
             final MediaPlayer mp = MediaPlayer.create(this, R.raw.normalhitclap);
             one.setOnClickListener(new OnClickListener(){
 
