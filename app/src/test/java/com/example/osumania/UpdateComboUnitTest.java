77f0158a48ad7baf25757e48409595bebb78e421
RaderF <36457068+RaderF@users.noreply.github.com>
2019-04-28 21:18:36 -0500

    IDK man some changes to things I don't understand

--- app/src/test/java/com/example/osumania/UpdateComboUnitTest.java
+++ app/src/test/java/com/example/osumania/UpdateComboUnitTest.java
@@ -7,14 +7,14 @@ public class UpdateComboUnitTest extends GameActivity{
 
     @Test
     public void increment_comboCount_works() {
-        comboCount = 0;
+        int comboCount = 0;
         updateCombo(true);
         assertEquals(1, comboCount);
     }
 
     @Test
     public void do_not_increment_comboCount_works() {
-        comboCount = 0;
+        int comboCount = 0;
         updateCombo(false);
         assertEquals(0, comboCount);
     }
